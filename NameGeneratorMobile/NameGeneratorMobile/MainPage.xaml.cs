eb3ee6e82a4430dd8608e7598c00620e1e221539
Marko <MarkoBachynsky1997@gmail.com>
2018-07-17 18:17:10 -0500

    Add project files.

---
+++ NameGeneratorMobile/NameGeneratorMobile/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NameGeneratorMobile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : CarouselPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+            string[] array = {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"};
+            ListViewNames.ItemsSource = array;
+            ListViewLists.ItemsSource = array;
+        }
+
+        private void ButtonGenerate_Clicked(object sender, EventArgs e)
+        {
+            string[] array1 = (string[])ListViewNames.ItemsSource;
+            string[] array2 = {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"};
+
+            string[] newArray = new string[array1.Length + array2.Length];
+            Array.Copy(array1, newArray, array1.Length);
+            Array.Copy(array2, 0, newArray, array1.Length, array2.Length);
+            ListViewNames.ItemsSource = newArray;
+            ListViewLists.ItemsSource = newArray;
+        }
+    }
+}
